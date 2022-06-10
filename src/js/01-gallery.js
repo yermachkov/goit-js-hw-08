edc43d8a756b0271158b4fa64c4a4613dca50da9
Alexey Yermachkov <o.yermachkov@gmail.com>
2022-06-10 23:21:33 +0300

    adds 1 task - done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+const createGallery = galleryItems.map(createGalleryMarkup).join('');
+galleryContainer.insertAdjacentHTML('beforeend', createGallery);
+
+
+function createGalleryMarkup ({ preview, original, description }) {
+  return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    `;
+};
+
+new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250
+    });
