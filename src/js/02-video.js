9c7e068a49773884058ddaa94f190f2e2d8962c1
Alexey Yermachkov <o.yermachkov@gmail.com>
2022-06-13 14:26:44 +0300

    adds 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const STORAGE_KEY = "videoplayer-current-time";
+const previousTime = localStorage.getItem(STORAGE_KEY);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(event) {
+  const time  = event.seconds;
+  localStorage.setItem(STORAGE_KEY, time);
+}
+
+player.setCurrentTime(previousTime).then().catch();
